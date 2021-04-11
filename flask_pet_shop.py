ac63ce4c15733770dc6165adb724561d0b0bd54f
Charlie Malcolm <charles.malcolm@octoenergy.com>
2021-04-11 17:13:29 +0100

    Adding pet shop data file and updating index route to include links to the animal route depending on the animal chosen

--- flask_pet_shop.py
+++ flask_pet_shop.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from helper import pets
 
 app = Flask(__name__)
 
@@ -10,13 +11,13 @@ def index():
     <p>Browse through the links below to find your new furry
     friend:</p>
     <ul>
-      <li>Dogs</li>
-      <li>Rabbits</li>
-      <li>Cats</li>
+      <li><a href='/animals/dogs'>Dogs</a></li>
+      <li><a href='/animals/rabbits'>Rabbits</a></li>
+      <li><a href='/animals/cats'>Cats</a></li>
     </ul>
     '''
 
-@app.route('/animals/pet_type')
+@app.route('/animals/<pet_type>')
 def animals(pet_type):
-  html = '<h1>List of Pets</h1>'
+  html = f'<h1>List of {pet_type}</h1>'
   return html
